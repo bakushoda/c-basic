4c03a44b07525c102a4555fe5d093dad3ecbd3ee
bakushoda <bshoda.ace@gmail.com>
2025-11-15 22:36:43 +0900

    add

---
+++ practice/4-suffer.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// int main(void) {
+//     printf("%d", 100+200);
+//     return 0;
+// }
+
+int main(void) {
+    printf("%d / %d = %d...%d\n", 40, 13, 40/13, 40%13);
+    return 0;
+}
